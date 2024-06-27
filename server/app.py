d8012d99322aafa2b28aa1b21da801775145de11
Favor Vincent <vfavorm77@gmail.com>
2024-06-27 13:23:25 +0300

    SQLALCHEMY lab complete

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -21,7 +20,43 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+      body = {"id" : earthquake.id,
+             "location" : earthquake.location,
+             "magnitude": earthquake.magnitude,
+             "year": earthquake.year}
+      status = 200
 
+    else:
+      body = {'message': f'Earthquake {id} not found.'}
+      status = 404
+    
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes = []
+    status = 200
+
+    for earthquake in earthquakes:
+       body = {"id" : earthquake.id,
+             "location" : earthquake.location,
+             "magnitude": earthquake.magnitude,
+             "year": earthquake.year}
+       quakes.append(body)
+    count = len(quakes)
+
+    response = {
+        "count": count,
+        "quakes": quakes
+    }
+
+    return make_response(response, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
